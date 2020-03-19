e1dd4eab9a2ad29e511225e018200ebbdc8b25f8
Abe Pazos <abe@hamoid.com>
2020-03-19 11:26:21 +0100

    Test Swing

---
+++ src/main/kotlin/AbeSwinging.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class AbeSwinging {
+    private JSlider a;
+    private JSlider b;
+    private JSlider c;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
