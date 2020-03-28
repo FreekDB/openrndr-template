66618dae9b0b47640d75e951212e3361553251c1
Abe Pazos <abe@hamoid.com>
2020-03-28 18:31:11 +0100

    Add gradients, use polar, other tweaks

--- src/main/kotlin/TestCurveOffset1.kt
+++ src/main/kotlin/TestCurveOffset1.kt
@@ -1,12 +1,13 @@
 import org.openrndr.KEY_ESCAPE
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.color.mix
 import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.shadestyles.linearGradient
 import org.openrndr.math.IntVector2
-import org.openrndr.math.Vector2
+import org.openrndr.math.Polar
 import org.openrndr.shape.SegmentJoin
 import org.openrndr.shape.ShapeContour
-import org.openrndr.shape.contour
 import kotlin.system.exitProcess
 
 /**
@@ -18,33 +19,48 @@ fun main() = application {
         width = 600
         height = 600
         hideWindowDecorations = true
-        position = IntVector2(10, 10)
     }
 
     program {
         val curves = mutableListOf<ShapeContour>()
 
+        // Make the core shape
         curves.add(
-            contour {
-                moveTo(Vector2(width * 0.5, height * 0.4))
-                lineTo(Vector2(width * 0.6, height * 0.6))
-                lineTo(Vector2(width * 0.4, height * 0.6))
-                lineTo(anchor)
-                close()
-            }
+            ShapeContour.fromPoints(
+                List(5) {
+                    Math.random() * 360
+                }.sorted().map { angle ->
+                    Polar(angle, 100.0).cartesian
+                }, true
+            )
         )
 
-        for(i in 0..6) {
-            curves.add(curves[i].offset(-50.0 + i * 7.0, SegmentJoin.BEVEL))
+        // Make offset shapes, each based on the previous one
+        for (i in 0..6) {
+            curves.add(curves[i].offset(-5.0 - i * 7.0, SegmentJoin.BEVEL))
         }
 
         extend(Screenshots())
 
         extend {
-            drawer.background(ColorRGBa.WHITE)
-            drawer.stroke = ColorRGBa(0.0, 0.0, 0.0, 0.1)
-            for(i in curves.size-1 downTo 0) {
-                drawer.fill = ColorRGBa(i * 0.15, 0.7, 1.0 - i * 0.18, 1.0)
+            drawer.background(ColorRGBa.fromHex(0x355C7D))
+            drawer.stroke = ColorRGBa.BLACK.opacify(0.1)
+            drawer.translate(width * 0.5, height * 0.5)
+
+            // Draw first larger shapes, then smaller ones
+            for (i in curves.size - 1 downTo 0) {
+                val percent = i / (curves.size - 1.0)
+                val a = mix(
+                    ColorRGBa.fromHex(0xF8B195),
+                    ColorRGBa.fromHex(0xF67280),
+                    percent
+                )
+                val b = mix(
+                    ColorRGBa.fromHex(0xC06C84),
+                    ColorRGBa.fromHex(0x6C5B7B),
+                    percent
+                )
+                drawer.shadeStyle = linearGradient(a, b, rotation = i * 185.0)
                 drawer.contour(curves[i])
             }
         }
