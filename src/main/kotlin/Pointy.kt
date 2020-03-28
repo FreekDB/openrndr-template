f323ed421aa39db86a107f027e2bd22ed7388e6e
Abe Pazos <abe@hamoid.com>
2020-03-28 18:32:08 +0100

    Avoid intersections in Pointy

--- src/main/kotlin/Pointy.kt
+++ src/main/kotlin/Pointy.kt
@@ -1,7 +1,12 @@
+import editablecurve.intersects
 import org.openrndr.KEY_ESCAPE
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.isolated
 import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.noise.Random.simplex
+import org.openrndr.extra.palette.PaletteStudio
+import org.openrndr.extra.shapes.regularStar
 import org.openrndr.math.IntVector2
 import org.openrndr.math.Vector2
 import org.openrndr.shape.ShapeContour
@@ -10,6 +15,7 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.random.Random
+import kotlin.system.exitProcess
 
 fun main() = application {
     configure {
@@ -24,10 +30,18 @@ fun main() = application {
 
         val points = List(100) {
             val a = 2 * PI * it / 100.0
-            Vector2(cos(a), sin(a)) * (200.0 + 80.0 * org.openrndr.extra.noise.Random.simplex(cos(a), sin(a)))
+            Vector2(cos(a), sin(a)) * (200.0 + 80.0 * simplex(cos(a), sin(a)))
         }
 
-        hairs.add(ShapeContour.fromPoints(points, true))
+        var star = regularStar(6, 50.0,
+            70.0, Vector2(0.0), 15.0)
+
+
+        var rp = PaletteStudio()
+        rp.randomPalette()
+        rp.randomPalette()
+
+        val mother = ShapeContour.fromPoints(points, true)
 
         extend(Screenshots()) {
             key = "s"
@@ -35,33 +49,42 @@ fun main() = application {
         extend {
 
             val v1 = Random.nextDouble()
-            val v2 = v1 + Random.nextDouble(0.05, 0.15)
-            val n1 = hairs[0].normal(v1)
-            val n2 = hairs[0].normal(v2)
-            val n = (n1 + n2).normalized
-            val p1 = hairs[0].position(v1)
-            val p2 = hairs[0].position(v2)
+            val v2 = (v1 + Random.nextDouble(0.05, 0.15)) % 1.0
+            val n1 = mother.normal(v1)
+            val n2 = mother.normal(v2)
+            val p1 = mother.position(v1)
+            val p2 = mother.position(v2)
             val d = p1.distance(p2)
             val c = contour {
                 moveTo(p1)
                 curveTo(p1 + n1 * d, p2 + n2 * d, p2)
             }
-            // TODO: verify that c doesn't cross any previous curves
-            hairs.add(c)
+            val hair = c.sampleLinear(5.0)
+            if(hairs.all { it.intersects(hair) == Vector2.INFINITY }) {
+                hairs.add(hair)
+            }
 
             drawer.background(ColorRGBa.PINK)
 
-            drawer.translate(width * 0.5, height * 0.5)
+            drawer.isolated {
+                drawer.translate(width * 0.5, height * 0.5)
+                drawer.fill = ColorRGBa.WHITE
+                drawer.stroke = ColorRGBa(0.0, 0.0, 0.0, 0.5)
+                drawer.contour(mother)
+                hairs.forEach { drawer.contour(it) }
+                drawer.contour(star)
+            }
 
-            drawer.fill = ColorRGBa.WHITE
-            drawer.stroke = ColorRGBa(0.0, 0.0, 0.0, 0.5)
-
-            hairs.forEach { drawer.contour(it) }
+            rp.palette.colors.forEachIndexed {
+                x, c ->
+                drawer.fill = c
+                drawer.rectangle(x * 15.0 + 10.0, height - 50.0, 10.0, 40.0)
+            }
         }
 
         keyboard.keyDown.listen {
             if (it.key == KEY_ESCAPE) {
-                application.exit()
+                exitProcess(0)
             }
         }
 
