45434f09072a9efead0641c5dd2e41bb7b86ee5a
Abe Pazos <abe@hamoid.com>
2020-03-30 22:54:01 +0200

    Program that crashes my computer without NoClear

---
+++ src/main/kotlin/p5Array.kt
@@ -0,0 +1,47 @@
+import org.openrndr.application
+import org.openrndr.color.rgb
+import org.openrndr.extra.noclear.NoClear
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
+
+// A port of a Processing example
+// https://github.com/processing/processing-docs/blob/master/content/examples/Basics/Arrays/Array/Array.pde
+
+fun main() = application {
+    configure {
+        width = 640
+        height = 360
+    }
+
+    program {
+        val coswave = List(width) { abs(cos(PI * it / width)) }
+
+        extend(extend(NoClear())) {
+            backdrop = {
+                var y1 = 0.0
+                var y2 = height / 3.0
+                coswave.forEachIndexed { x, v ->
+                    drawer.stroke = rgb(v)
+                    drawer.lineSegment(x.toDouble(), y1, x.toDouble(), y2)
+                }
+
+                y1 = y2
+                y2 = y1 + y1
+                coswave.forEachIndexed { x, v ->
+                    drawer.stroke = rgb(v / 4)
+                    drawer.lineSegment(x.toDouble(), y1, x.toDouble(), y2)
+                }
+
+                y1 = y2
+                y2 = height * 1.0
+                coswave.forEachIndexed { x, v ->
+                    drawer.stroke = rgb(1 - v)
+                    drawer.lineSegment(x.toDouble(), y1, x.toDouble(), y2)
+                }
+            }
+        }
+        extend {
+        }
+    }
+}
