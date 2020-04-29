0f824b67b3987730df5c6cfbd0f839f8b51064f9
Abe Pazos <abe@hamoid.com>
2020-04-29 16:51:24 +0200

    Vector2.distance replaced by built-in .distanceTo

--- src/main/kotlin/geometry/helpers.kt
+++ src/main/kotlin/geometry/helpers.kt
@@ -2,6 +2,24 @@ package geometry
 
 import org.openrndr.math.Vector2
 
-fun Vector2.distance(other: Vector2): Double {
-    return (this - other).length
+/*
+PVector intersectRays(Ray a, Ray b) {
+  PVector d = PVector.sub(b.start, a.start);
+  float det = b.direction.x * a.direction.y - b.direction.y * a.direction.x;
+  if (det != 0) {
+    float u = (d.y * b.direction.x - d.x * b.direction.y) / det;
+    float v = (d.y * a.direction.x - d.x * a.direction.y) / det;
+    if (u > 0 && v > 0) {
+      // front side
+      fill(255);
+      return PVector.add(a.start, PVector.mult(a.direction, u));
+    }
+    if (u < 0 && v < 0) {
+      // backside
+      fill(#FF0000);
+      return PVector.add(a.start, PVector.mult(a.direction, u));
+    }
+  }
+  return new PVector(); // Deal with this later
 }
+*/
