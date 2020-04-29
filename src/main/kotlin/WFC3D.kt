a8b7d7864d653aef1b6194fa723dddb6fde90f1d
Abe Pazos <abe@hamoid.com>
2020-04-29 16:58:40 +0200

    Start WFC port from OF

---
+++ src/main/kotlin/WFC3D.kt
@@ -0,0 +1,38 @@
+import data.Array3D
+import org.openrndr.KEY_ESCAPE
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.loadFont
+import org.openrndr.extensions.Screenshots
+import org.openrndr.math.Vector2
+import org.openrndr.shape.LineSegment
+import org.openrndr.shape.Segment
+import org.openrndr.shape.intersection
+import kotlin.system.exitProcess
+
+/**
+ * Basic template
+ */
+
+fun main() = application {
+    configure {
+        width = 1500
+        height = 800
+    }
+
+    program {
+        val numbers = Array3D(5, 5, 5, 0.0)
+        numbers[2, 2, 2] = 0.5
+        println(numbers)
+
+        extend(Screenshots())
+        extend {
+        }
+
+        keyboard.keyDown.listen {
+            when (it.key) {
+                KEY_ESCAPE -> exitProcess(0)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/data/Array3D.kt
@@ -0,0 +1,17 @@
+package data
+
+data class Array3D<T>(private val width: Int, private val height: Int, private val depth: Int, private val defaultVal: T) {
+    private val data = MutableList(width * height * depth) { defaultVal }
+
+    operator fun get(i: Int, j: Int, k: Int): T {
+        return data[i * width * height + j * depth + k]
+    }
+
+    operator fun set(i: Int, j: Int, k: Int, v: T) {
+        data[i * width * height + j * depth + k] = v
+    }
+
+    fun size() {
+        data.size
+    }
+}
