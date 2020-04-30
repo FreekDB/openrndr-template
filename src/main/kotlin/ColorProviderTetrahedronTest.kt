7e20ca5f9f5e72813445a16c564b56d0aed042f9
Abe Pazos <abe@hamoid.com>
2020-04-30 10:56:32 +0200

    Test ColorProviderTetrahedron Lab vs RGB

---
+++ src/main/kotlin/ColorProviderTetrahedronTest.kt
@@ -0,0 +1,103 @@
+import color.ColorProviderTetrahedron
+import math.doubleExponentialSigmoid
+import org.openrndr.KEY_ENTER
+import org.openrndr.KEY_ESCAPE
+import org.openrndr.KEY_INSERT
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.shadestyles.linearGradient
+import org.openrndr.math.Vector3
+import org.openrndr.panel.ControlManager
+import org.openrndr.panel.elements.button
+import org.openrndr.panel.elements.div
+import org.openrndr.panel.elements.requestRedraw
+import org.openrndr.panel.layout
+import org.openrndr.panel.style.*
+import org.openrndr.panel.styleSheet
+import kotlin.math.pow
+import kotlin.system.exitProcess
+
+/**
+ * Testing Lab and RGB color mixing.
+ * Based on these 3 palettes, RGB mixing is more pleasant
+ * Lab seems to produce darker results
+ */
+
+fun main() = application {
+    configure {
+        width = 1500
+        height = 800
+    }
+
+    program {
+        var modeRGB = true;
+        val palettes = listOf(
+            ColorProviderTetrahedron("E1AA66", "8E4770", "7F615B", "8CA9B6", "A"),
+            ColorProviderTetrahedron("302739", "3C8C9E", "82BBB6", "E1C367", "B"),
+            ColorProviderTetrahedron("279193", "B1F8C9", "F5C579", "EC3F4F", "C"),
+            ColorProviderTetrahedron("5B4C4D", "A7A994", "CFC9A7", "D04F27", "D")
+        )
+        var palette = palettes[0]
+
+        extend(ControlManager()) {
+            styleSheet(has class_ "horizontal") {
+                display = Display.FLEX
+                flexDirection = FlexDirection.Row
+            }
+            layout {
+                div("horizontal") {
+                    button(label = "RGB") {
+                        events.clicked.listen {
+                            modeRGB = !modeRGB
+                            this.label = if (modeRGB) "RGB" else "Lab"
+                            requestRedraw()
+                        }
+                    }
+                    button(label = "Palette") {
+                        events.clicked.listen {
+                            Random.seed = System.currentTimeMillis().toString()
+                            palette = Random.pick(palettes)
+                            this.label = "Palette ${palette.name}"
+                            requestRedraw()
+                        }
+                    }
+                }
+            }
+        }
+        extend(Screenshots())
+        extend {
+            Random.resetState()
+            for (x in 0 until width step 50) {
+                for (y in 0 until width step 50) {
+                    //val p = Vector3(Random.double0(1.0), Random.double0(1.0), Random.double0(1.0))
+                    val p = Vector3(
+                        doubleExponentialSigmoid(x * 0.001, 0.0),
+                        doubleExponentialSigmoid(0.0, y * 0.001),
+                        doubleExponentialSigmoid(x * 0.001, y * 0.001)
+                    )
+                    drawer.stroke = null //ColorRGBa.WHITE.opacify(0.7)
+                    drawer.fill = if (modeRGB) palette.getColorViaRGB(p) else palette.getColor(p)
+                    drawer.shadeStyle = linearGradient(
+                        ColorRGBa.WHITE,
+                        ColorRGBa.WHITE.shade(0.7),
+                        rotation = Random.value(x * 0.001, y * 0.001) * 180 + 180,
+                        exponent = 4.0
+                    )
+                    drawer.rectangle(x * 1.0, y * 1.0, 50.0, 50.0)
+                }
+            }
+        }
+
+        keyboard.keyDown.listen {
+            when (it.key) {
+                KEY_ESCAPE -> exitProcess(0)
+            }
+        }
+    }
+}
+
+//private operator fun Vector3.plus(d: Double): Vector3 {
+//    return Vector3(this.x + d, this.y + d, this.z + d)
+//}
--- src/main/kotlin/math/helpers.kt
+++ src/main/kotlin/math/helpers.kt
@@ -1,6 +1,20 @@
 package math
 
+import org.openrndr.extra.noise.Random
+import kotlin.math.pow
+
 fun angleDiff(a: Double, b: Double): Double {
     val dist: Double = (a - b + 360) % 360
     return if (dist > 180) dist - 360 else dist
+}
+
+fun doubleExponentialSigmoid(min: Double, max: Double): Double {
+    val x = Random.simplex(min, max) * 0.5 + 0.5
+    val a = 0.15
+
+    return if (x <= 0.5) {
+        ((2.0 * x).pow(1.0 / a)) / 2.0;
+    } else {
+        1.0 - ((2.0 * (1.0 - x)).pow(1.0 / a)) / 2.0;
+    }
 }
