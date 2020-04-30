4eb1928d1e0da3372d68d84424470117d57419c9
Abe Pazos <abe@hamoid.com>
2020-04-30 11:02:14 +0200

    Next version of Pointy

--- src/main/kotlin/Pointy.kt
+++ src/main/kotlin/Pointy.kt
@@ -1,10 +1,10 @@
 import editablecurve.ensureExtension
 import editablecurve.intersects
-import geometry.distance
-import org.openrndr.KEY_ESCAPE
-import org.openrndr.KEY_INSERT
-import org.openrndr.application
+import geometry.noisified
+import org.openrndr.*
+import org.openrndr.color.ColorLUVa
 import org.openrndr.color.ColorRGBa
+import org.openrndr.color.ColorXSVa
 import org.openrndr.dialogs.saveFileDialog
 import org.openrndr.draw.isolated
 import org.openrndr.extensions.Screenshots
@@ -21,6 +21,19 @@ import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.system.exitProcess
 
+/**
+ * 1. Create main shape - mother
+ * 2. Draw normals point out from mother
+ * 3. Connect two normals with a bezier curve, creating petals
+ * 4. Make sure the petals don't cross with each other. Being inside each other is ok though.
+ * 5. Instead of fully random locations, sometimes suggest petals slightly inside or outside
+ *    other petals.
+ * 6. This leads to too many lines close to each other. I could create a list of slots.
+ *    Mother will have 1000 slots, for example. When we consume all slots, we are done.
+ *    When figuring out which slots to use the approah would be similar to the current one:
+ *    either pick two random ones with a maximum separation, or pick adjacent ones to an
+ *    existing line. Basically it would be like the current usage of `used`, but with integers
+ */
 fun main() = application {
     configure {
         width = 15 * 60
@@ -28,14 +41,20 @@ fun main() = application {
     }
 
     program {
-        var hairs = mutableListOf<ShapeContour>()
+        var center = drawer.bounds.center
+        var rotation = 0.0
+        var bgcolor = ColorRGBa.PINK
 
-        val used = mutableListOf<Pair<Double, Double>>()
-        val seed = System.currentTimeMillis()
+        val maxNumOfHairs = 360
+        var hairContours = mutableListOf<ShapeContour>()
+        val hairLocations = mutableListOf<Pair<Int, Int>>()
+        val motherSlots = MutableList(maxNumOfHairs) { false }
+
+        val seed = System.currentTimeMillis().toInt()
 
         val mother = ShapeContour.fromPoints(List(100) {
             val a = 2 * PI * it / 100.0
-            Polar(Math.toDegrees(a), 200.0 + 80.0 * simplex(seed.toInt(), cos(a), sin(a))).cartesian
+            Polar(Math.toDegrees(a), 200.0 + 80.0 * simplex(seed, cos(a), sin(a))).cartesian
         }, true)
 
         fun exportSVG() {
@@ -44,7 +63,7 @@ fun main() = application {
             svg.fill = null
             svg.stroke = ColorRGBa.BLACK
             svg.contour(mother)
-            svg.contours(hairs)
+            svg.contours(hairContours)
             saveFileDialog(supportedExtensions = listOf("svg")) {
                 it.writeText(writeSVG(svg.composition))
             }
@@ -52,43 +71,66 @@ fun main() = application {
 
         extend(Screenshots())
         extend {
-            val jmp = 0.01 + seconds * 0.02
-            var v1 : Double
-            var v2 : Double
-            if (Random.bool(0.3) || used.isEmpty()) {
-                v1 = Random.double0()
-                v2 = (v1 + Random.double(0.01, jmp * 3)) % 1.0
-            } else {
-                val v = Random.pick(used)
-                val offset = Random.double(-0.05, 0.05)
-                v1 = v.first + offset
-                v2 = v.second - offset
-            }
-            val n1 = mother.normal(v1)
-            val n2 = mother.normal(v2)
-            val p1 = mother.position(v1)
-            val p2 = mother.position(v2)
+            if(hairContours.size < maxNumOfHairs - 1) {
+                // figure out which slots to connect
+                val jmp = (15 + seconds * 20).toInt()
+                var v1: Int
+                var v2: Int
+                if (Random.bool(0.3) || hairContours.isEmpty()) {
+                    v1 = Random.int0(maxNumOfHairs)
+                    v2 = (v1 + Random.int(1, jmp)) % maxNumOfHairs
+                } else {
+                    val v = Random.pick(hairLocations)
+                    val offset = Random.int(-50, 50)
+                    v1 = (v.first + offset + maxNumOfHairs) % maxNumOfHairs
+                    v2 = (v.second - offset + maxNumOfHairs) % maxNumOfHairs
+                }
 
-            val d = p1.distance(p2)
-            val side = Random.int0(2) * 2.0 - 1.0
-            val c = contour {
-                moveTo(p1)
-                curveTo(p1 + n1 * d * side, p2 + n2 * d * side, p2)
-            }
-            val hair = c.sampleLinear(1.0)
-            if (hairs.all { it.intersects(hair) == Vector2.INFINITY }) {
-                hairs.add(hair)
-                used.add(Pair(v1, v2))
-            }
+                if(!motherSlots[v1] && !motherSlots[v2] && v1 != v2) {
+                    val normalize = 1.0 / maxNumOfHairs
+                    val n1 = mother.normal(v1 * normalize)
+                    val n2 = mother.normal(v2 * normalize)
+                    val p1 = mother.position(v1 * normalize)
+                    val p2 = mother.position(v2 * normalize)
 
-            drawer.background(ColorRGBa.PINK)
+                    val d = p1.distanceTo(p2)
+                    val side = Random.int0(2) * 2.0 - 1.0
+                    val c = contour {
+                        moveTo(p1)
+                        curveTo(p1 + n1 * d * side, p2 + n2 * d * side, p2)
+                    }
+                    val hair = c.sampleLinear(1.0)
+                    if (hairContours.all { it.intersects(hair) == Vector2.INFINITY }) {
+                        hairContours.add(hair)
+                        if(Random.bool(0.2)) {
+                            for(i in 1..5) {
+                                hairContours.add(hair.noisified(i, false, 0.01))
+                            }
+                        }
+                        hairLocations.add(Pair(v1, v2))
+                        motherSlots[v1] = true
+                        motherSlots[v2] = true
+                    }
+                }
+            }
 
             drawer.isolated {
-                drawer.translate(drawer.bounds.center)
-                drawer.fill = null
-                drawer.stroke = ColorRGBa(0.0, 0.0, 0.0, 0.5)
-                drawer.contour(mother)
-                drawer.contours(hairs)
+                background(bgcolor)
+                translate(center)
+                rotate(rotation)
+                fill = null
+                stroke = ColorRGBa(0.0, 0.0, 0.0, 0.5)
+                contour(mother)
+                contours(hairContours)
+            }
+        }
+
+        mouse.dragged.listen {
+            if(mouse.pressedButtons.contains(MouseButton.LEFT)) {
+                center += it.dragDisplacement
+            }
+            if(mouse.pressedButtons.contains(MouseButton.RIGHT)) {
+                rotation += it.dragDisplacement.x
             }
         }
 
@@ -96,6 +138,7 @@ fun main() = application {
             when (it.key) {
                 KEY_ESCAPE -> exitProcess(0)
                 KEY_INSERT -> exportSVG()
+                KEY_ENTER -> bgcolor = ColorXSVa(Random.double0(360.0), 0.3, 0.95).toRGBa()
             }
         }
     }
