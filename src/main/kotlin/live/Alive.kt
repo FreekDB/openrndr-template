79268e5bcfe7f15a01438290387a77e45c416710
Abe Pazos <abe@hamoid.com>
2020-05-12 17:50:40 +0200

    New olive style

---
+++ src/main/kotlin/live/Alive.kt
@@ -0,0 +1,30 @@
+package live
+
+import org.openrndr.application
+import org.openrndr.color.rgb
+import org.openrndr.color.rgba
+import org.openrndr.extra.olive.oliveProgram
+import kotlin.math.cos
+
+fun main() {
+    application {
+        configure {
+            width = 900
+            height = 900
+        }
+        oliveProgram {
+            extend {
+                drawer.clear(rgb(0.1, 0.5, 0.3))
+                drawer.stroke = null
+                for (i in 0 until 100) {
+                    drawer.fill = rgba(1.0, 1.0, 1.0, i / 100.0)
+                    drawer.circle(
+                        width / 2.0 + cos(seconds + i) * 320.0,
+                        80.0+ i * 7.2,
+                        cos(i + seconds * 0.5) * 20.0 + 20.0
+                    )
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/live/TemplateLiveProgram.kt
+++ src/main/kotlin/live/TemplateLiveProgram.kt
@@ -1,24 +1,24 @@
 package live
 
-import org.openrndr.Program
-import org.openrndr.application
-import org.openrndr.extra.olive.Olive
+//import org.openrndr.Program
+//import org.openrndr.application
+//import org.openrndr.extra.olive.Olive
 
 /**
  *  This is a template for a live program.
- *  The first you will run this program it will create a script file at src/main/kotlin/live.kts
+ *  The first you will run this program it will create a script file at src/main/kotlin/live.kaats
  *  This script file can be modified while the program is running.
  *
  *  Please refer to https://guide.openrndr.org/#/10_OPENRNDR_Extras/C03_Live_coding for more
  *  instructions on using the live coding environment.
  */
 
-fun main() = application {
-    configure {
-        width = 400
-        height = 400
-    }
-    program {
-        extend(Olive<Program>())
-    }
-}
+//fun main() = application {
+//    configure {
+//        width = 400
+//        height = 400
+//    }
+//    program {
+//        extend(Olive<Program>())
+//    }
+//}
