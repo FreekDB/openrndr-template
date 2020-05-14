c44a8abe37d5cf36e625300bb4fab0c76205c6ce
Abe Pazos <abe@hamoid.com>
2020-05-14 12:45:57 +0200

    Update convex hull

--- src/main/kotlin/geometry/helpers.kt
+++ src/main/kotlin/geometry/helpers.kt
@@ -1,40 +1,83 @@
 package geometry
 
 import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+import kotlin.math.abs
 
 /**
  * Calculate the convex hull of a list of 2D points
  * https://rosettacode.org/wiki/Convex_hull#Kotlin
  */
-// counter-clockwise turn
-fun cw(a: Vector2, b: Vector2, c: Vector2) =
-    ((b.x - a.x) * (c.y - a.y)) < ((b.y - a.y) * (c.x - a.x))
-
 @ExperimentalStdlibApi
 fun convexHull(p: List<Vector2>): List<Vector2> {
     if (p.size <= 3) return p
     val sorted = p.sortedBy { it.x }
     val result = mutableListOf<Vector2>()
 
-    sorted.forEach {
-        result.run {
-            while (size >= 2 && cw(this[size - 2], last(), it)) {
-                removeLast()
-            }
-            add(it)
+    fun clockWise(a: Vector2, b: Vector2, c: Vector2) =
+        ((b.x - a.x) * (c.y - a.y)) < ((b.y - a.y) * (c.x - a.x))
+
+    fun MutableList<Vector2>.tryAdd(point: Vector2, cutIndex: Int) {
+        while (size >= cutIndex && clockWise(this[size - 2], last(), point)) {
+            removeLast()
         }
+        add(point)
     }
 
-    val t = result.size + 1
+    sorted.forEach {
+        result.tryAdd(it, 2)
+    }
+
+    val cutIndex = result.size + 1
     for (i in sorted.size - 2 downTo 0) {
-        val it = sorted[i]
-        result.run {
-            while (size >= t && cw(this[size - 2], last(), it)) {
-                removeLast()
-            }
-            add(it)
-        }
+        result.tryAdd(sorted[i], cutIndex)
     }
     result.removeLast()
     return result
+}
+
+/**
+ * Separate Vector2 points
+ */
+fun List<Vector2>.separate(separation: Double): List<Vector2> {
+    return this.map { me ->
+        var sum = Vector2.ZERO
+        var count = 0
+        this.forEach { other ->
+            val d = me.distanceTo(other)
+            if (d < separation) {
+                var force = (me - other).normalized
+                force /= abs(d)
+                sum += force
+                count++;
+            }
+        }
+        if (count > 0) {
+            sum = sum.normalized
+        }
+        me + sum
+    }
+}
+
+/**
+ * Separate Vector2 points
+ */
+fun List<Circle>.separated(separation: Double): List<Circle> {
+    return this.map { me ->
+        var sum = Vector2.ZERO
+        var count = 0
+        this.forEach { other ->
+            val d = me.center.distanceTo(other.center) - me.radius - other.radius
+            if (d < separation) {
+                var force = (me.center - other.center).normalized
+                force /= abs(d)
+                sum += force
+                count++;
+            }
+        }
+        if (count > 0) {
+            sum = sum.normalized
+        }
+        Circle(me.center + sum, me.radius)
+    }
 }
