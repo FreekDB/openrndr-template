4fd5d8d81435a252434275f0eec3aca9d1034a61
Abe Pazos <abe@hamoid.com>
2020-06-26 16:24:23 +0200

    Convert tangents into extension function

--- src/main/kotlin/apps/two_circles_line_tangents.kt
+++ src/main/kotlin/apps/two_circles_line_tangents.kt
@@ -24,7 +24,7 @@ fun main() = application {
                 clear(ColorRGBa.WHITE)
                 circle(c0)
                 circle(c1)
-                getTangents(c0, c1).forEach {
+                c0.tangents(c1).forEach {
                     lineSegment(it)
                     circle(it.start, 5.0)
                     circle(it.end, 5.0)
@@ -34,19 +34,19 @@ fun main() = application {
     }
 }
 
-fun getTangents(c0: Circle, c1: Circle): List<LineSegment> {
+fun Circle.tangents(other: Circle): List<LineSegment> {
     val result = mutableListOf<LineSegment>()
-    val distSq = c0.center.squaredDistanceTo(c1.center)
-    if (distSq > (c0.radius - c1.radius) * (c0.radius - c1.radius)) {
+    val distSq = center.squaredDistanceTo(other.center)
+    if (distSq > (radius - other.radius) * (radius - other.radius)) {
         val d = sqrt(distSq)
-        val v = (c1.center - c0.center) / d
+        val v = (other.center - center) / d
         for (sign1 in arrayOf(1.0, -1.0)) {
-            val c = (c0.radius - sign1 * c1.radius) / d
+            val c = (radius - sign1 * other.radius) / d
             if (c * c <= 1.0) {
                 val h = sqrt(max(0.0, 1.0 - c * c))
                 for (sign2 in arrayOf(1.0, -1.0)) {
                     val n = v * c + Vector2(-v.y, v.x) * sign2 * h
-                    result.add(LineSegment(c0.center + n * c0.radius, c1.center + n * sign1 * c1.radius))
+                    result.add(LineSegment(center + n * radius, other.center + n * sign1 * other.radius))
                 }
             }
         }
