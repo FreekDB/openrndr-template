9d2160c1ebf79892c6881f64405b2a1ba6ff4727
Abe Pazos <abe@hamoid.com>
2020-06-27 11:59:42 +0200

    Simplify NoJitter.kt

--- src/main/kotlin/extensions/NoJitter.kt
+++ src/main/kotlin/extensions/NoJitter.kt
@@ -8,13 +8,10 @@ import org.openrndr.extra.fx.color.SetBackground
 class NoJitter : Extension {
     override var enabled: Boolean = true
 
-    private val rt = renderTarget(16, 16) {
-        colorBuffer()
-    }
-    private val filtered = colorBuffer(16, 16)
-    private val simpleFilter = SetBackground()
+    private val a = colorBuffer(16, 16)
+    private val b = colorBuffer(16, 16)
 
     override fun beforeDraw(drawer: Drawer, program: Program) {
-        simpleFilter.apply(rt.colorBuffer(0), filtered)
+        a.copyTo(b)
     }
 }
