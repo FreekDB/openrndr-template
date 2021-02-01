744300048ab1495ac492d4bbbf057461448c4eab
Abe Pazos <abe@hamoid.com>
2021-02-01 13:47:58 +0100

    Update voronoi (alienheads) and add new sketch

---
+++ src/main/kotlin/latest/glowingIntersectionOfCircles.kt
@@ -0,0 +1,80 @@
+package latest
+
+import aBeLibs.data.uniquePairs
+import aBeLibs.geometry.intersections
+import aBeLibs.random.pickWeighted
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.color.rgb
+import org.openrndr.draw.colorBuffer
+import org.openrndr.draw.isolatedWithTarget
+import org.openrndr.draw.renderTarget
+import org.openrndr.extra.fx.blur.GaussianBloom
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.noise.gradientPerturbFractal
+import org.openrndr.extra.noise.simplex
+import org.openrndr.ffmpeg.ScreenRecorder
+import org.openrndr.math.Polar
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+
+private data class ColoredCircle(
+    val color: ColorRGBa = rgb(listOf("D1313D", "E5625C", "F9BF76", "8EB2C5", "615375").random()),
+    var circle: Circle = Circle(Vector2.ZERO,
+        listOf(25.0, 50.0, 200.0).pickWeighted(listOf(10.0, 5.0, 1.0))
+    )
+)
+
+fun main() = application {
+    configure {
+        width = 720
+        height = 721
+    }
+
+    program {
+        val circles = List(20) { ColoredCircle() }
+        val dry = renderTarget(width, height) {
+            colorBuffer()
+        }
+        val wet = colorBuffer(width, height)
+        val glow = GaussianBloom().apply {
+            window = 14
+            sigma = 0.05
+            gain = 10.0
+        }
+
+        extend(ScreenRecorder())
+        extend {
+            drawer.clear(ColorRGBa.BLACK)
+            drawer.circles {
+                circles.forEachIndexed { i, it ->
+                    fill = null
+                    stroke = it.color
+                    strokeWeight = 2.0
+                    val p = gradientPerturbFractal(i, position = Vector2(
+                        i * 0.1, seconds * 0.002))
+                    val theta = seconds * (i - 4) * 0.4 + simplex(i, p) * 180
+                    val radius = simplex(i, p.copy(x = p.x + 0.2)) * 150 + 200
+                    it.circle = it.circle.movedTo(
+                        drawer.bounds.center + Polar(theta, radius).cartesian
+                    )
+                    circle(it.circle.scaledTo(it.circle.radius + 2))
+                }
+            }
+            drawer.isolatedWithTarget(dry) {
+                clear(ColorRGBa.TRANSPARENT)
+                fill = ColorRGBa.WHITE.shade(Random.double(0.4, 1.0))
+                stroke = null
+                val intersections = mutableListOf<Circle>()
+                circles.uniquePairs().forEach {
+                    val (first, second) = it.toList()
+                    intersections.addAll(first.circle.intersections(second.circle)
+                        .map { pos -> Circle(pos, 5.0) })
+                }
+                circles(intersections)
+            }
+            glow.apply(dry.colorBuffer(0), wet)
+            drawer.image(wet)
+        }
+    }
+}
--- src/main/kotlin/latest/voronoi01.kt
+++ src/main/kotlin/latest/voronoi01.kt
@@ -1,51 +1,72 @@
 package latest
 
+import aBeLibs.geometry.dedupe
+import org.openrndr.KEY_ENTER
+import org.openrndr.KEY_ESCAPE
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.extensions.SingleScreenshot
 import org.openrndr.extra.noise.Random
+import org.openrndr.extra.noise.gradientPerturbFractal
 import org.openrndr.extra.noise.poissonDiskSampling
 import org.openrndr.extra.triangulation.Delaunay
-import org.openrndr.math.Vector2
 import org.openrndr.shape.Circle
-import org.openrndr.shape.Rectangle
+import org.openrndr.shape.draw
+import org.openrndr.shape.drawComposition
+import org.openrndr.svg.saveToFile
+import org.openrndr.utils.namedTimestamp
+import java.io.File
 
 fun main() {
     application {
         configure {
-            width = 800
-            height = 800
+            width = 900
+            height = 900
         }
         program {
-            if (System.getProperty("takeScreenshot") == "true") {
-                extend(SingleScreenshot()) {
-                    this.outputFile = System.getProperty("screenshotPath")
+            val svg = drawComposition { }
+
+            fun newDesign() {
+                val circle = Circle(drawer.bounds.center, width * 0.32)
+                val points = poissonDiskSampling(
+                    width * 1.0, height * 1.0, 15.0
+                ) { _, _, v ->
+                    val perturb = gradientPerturbFractal(
+                        seconds.toInt(),
+                        position = v * 0.008
+                    )
+                    Random.simplex(perturb) < Random.double(-1.0, 0.0)
+                }
+                val delaunay = Delaunay.from(
+                    points + circle.contour.equidistantPositions(40)
+                )
+                val voronoi = delaunay.voronoi(drawer.bounds.scale(0.8))
+
+                svg.clear()
+                svg.draw {
+                    fill = null
+                    stroke = ColorRGBa.PINK
+                    contours(voronoi.cellsPolygons())
                 }
             }
 
-            val circle = Circle(Vector2(400.0), 250.0)
-            val frame = Rectangle.fromCenter(Vector2(400.0), 600.0, 600.0)
-
-            val points = poissonDiskSampling(
-                width * 1.0,
-                height * 1.0,
-                15.0
-            ) { _, _, v ->
-                circle.contains(v) && Random.simplex(v * 0.005) > 0.0
-            }
-
-            val delaunay =
-                Delaunay.from(points + circle.contour.equidistantPositions(40))
-            val voronoi = delaunay.voronoi(frame)
-
-            val cells = voronoi.cellsPolygons()
-
             extend {
                 drawer.clear(ColorRGBa.BLACK)
+                drawer.composition(svg)
+            }
 
-                drawer.fill = null
-                drawer.stroke = ColorRGBa.PINK
-                drawer.contours(cells)
+            mouse.buttonDown.listen {
+                newDesign()
+            }
+
+            keyboard.keyDown.listen {
+                when (it.key) {
+                    KEY_ENTER -> {
+                        val fileName = program.namedTimestamp("svg", "print")
+                        svg.dedupe().saveToFile(File(fileName))
+                        println("Saved as $fileName")
+                    }
+                    KEY_ESCAPE -> application.exit()
+                }
             }
         }
     }
