d2db6196df15815ee70d859af0e914c1a1730ff9
Abe Pazos <abe@hamoid.com>
2021-02-01 13:45:52 +0100

    Explain why uniquePairs uses Set and not Pair

--- src/main/kotlin/aBeLibs/data/uniquePairs.kt
+++ src/main/kotlin/aBeLibs/data/uniquePairs.kt
@@ -11,3 +11,21 @@ fun <E> Collection<E>.uniquePairs(): MutableSet<Set<E>> {
     }
     return result
 }
+
+/*
+// This looks nicer but doesn't work for my purposes because
+// Pair(4, 8) != Pair(8, 4)
+// but
+// setOf(4, 8) == setOf(8, 4)
+fun <E> Collection<E>.uniquePairs(): MutableSet<Pair<E, E>> {
+    val result: MutableSet<Pair<E, E>> = LinkedHashSet()
+    this.forEach { a ->
+        this.forEach { b ->
+            if(a != b) {
+                result.add(Pair(a, b))
+            }
+        }
+    }
+    return result
+}
+*/
