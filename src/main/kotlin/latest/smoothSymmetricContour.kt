88211aed9458b341e46af4bcf0e4380ab9220b73
Abe Pazos <abe@hamoid.com>
2021-02-15 11:11:23 +0100

    More flexible .symmetrize (takes a lambda instead of Double)

--- src/main/kotlin/latest/smoothSymmetricContour.kt
+++ src/main/kotlin/latest/smoothSymmetricContour.kt
@@ -6,8 +6,8 @@ import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
 import org.openrndr.extensions.Screenshots
 import org.openrndr.extra.noise.Random
+import org.openrndr.math.IntVector2
 import org.openrndr.math.Vector2
-import org.openrndr.math.map
 import org.openrndr.shape.*
 import java.lang.Double.min
 
@@ -15,30 +15,32 @@ import java.lang.Double.min
  * In this program I'm going to work with a contour.
  * I want to do something similar to .fromPoints() but
  * I don't want a polygonal look, but smoothed instead.
- * I can thing of 4 approaches:
- * - completely automatic
- * - set the width for each vertex
- * - set the width on each side of each vertex
- * - set widths and tilt
+ * The current version generates the tangent lines, which is
+ * not necessary and makes it more complicated, but I wanted
+ * to show those lines when holding the left shift key down.
  */
 
 fun main() = application {
     configure {
-        width = 1500
-        height = 400
+        width = 1000
+        height = 1000
     }
     program {
-        fun ShapeContour.symmetrize(roundness: Double = 0.1): Pair<ShapeContour, List<LineSegment>> {
+        fun ShapeContour.symmetrize(roundness: (Int) -> Pair<Double, Double>):
+                Pair<ShapeContour, List<LineSegment>> {
             val visibleTangents = mutableListOf<LineSegment>()
             val tangents = segments.mapIndexed { i, curr ->
                 val next = segments[(i - 1 + segments.size) % segments.size]
                 (curr.direction()).mix(next.direction(), 0.5).normalized
             }
             val newSegments = segments.mapIndexed { i, currSegment ->
-                val l = currSegment.length
-                val j = (i + 1 + segments.size) % segments.size
-                val c0 = currSegment.start + tangents[i] * l * roundness
-                val c1 = currSegment.end - tangents[j] * l * roundness
+                val sz = segments.size
+                val len = currSegment.length
+                val iNext = (i + 1 + sz) % sz
+                val c0 = currSegment.start +
+                        tangents[i] * len * roundness(i).first
+                val c1 = currSegment.end -
+                        tangents[iNext] * len * roundness((i + 1) % sz).second
                 visibleTangents.add(LineSegment(currSegment.start, c0))
                 visibleTangents.add(LineSegment(c1, currSegment.end))
                 visibleTangents.add(
@@ -54,22 +56,27 @@ fun main() = application {
 
         fun softies(): List<Pair<ShapeContour, List<LineSegment>>> {
             do {
-                val numPoints = Random.int(3, 8)
+                val numPoints = Random.int(6, 8)
                 val points = List(numPoints) {
                     Rectangle.fromCenter(Vector2.ZERO, 300.0).randomPoint()
                 }
                 val center = points.bounds.center
+                val maxSize = 120.0
                 val scale = min(
-                    250 / points.bounds.width,
-                    250 / points.bounds.height
+                    maxSize / points.bounds.width,
+                    maxSize / points.bounds.height
                 )
                 val pointsCentered = points.map { (it - center) * scale }
                 val c = ShapeContour.fromPoints(pointsCentered, true)
                 // NOTE: .intersections() can return 0 intersections even if
                 // there is one: https://github.com/lacuna/artifex/issues/3
                 if (c.intersections(c).isEmpty()) {
-                    return List(5) {
-                        c.symmetrize(0.05 + it * 0.1)
+                    return List(16) {
+                        c.symmetrize { i ->
+                            val n =
+                                Random.simplex(it * 0.3, i * 0.3) * 0.4 + 0.5
+                            Pair(n, n)
+                        }
                     }
                 }
             } while (true)
@@ -78,19 +85,15 @@ fun main() = application {
         var softies = softies()
         extend(Screenshots())
         extend {
-            drawer.apply {
-                clear(ColorRGBa.WHITE)
-                fill = ColorRGBa.PINK
-                softies.forEachIndexed { i, it ->
-                    isolated {
-                        translate(
-                            i.toDouble().map(
-                                0.0, softies.size - 1.0,
-                                150.0, width - 150.0
-                            ), height * 0.5
-                        )
-                        stroke = null
-                        contour(it.first)
+            drawer.clear(ColorRGBa.WHITE)
+            drawer.fill = ColorRGBa.PINK
+            softies.forEachIndexed { i, it ->
+                drawer.isolated {
+                    val pos = (IntVector2(i % 4, i / 4).vector2 + 1.0) / 5.0
+                    translate(bounds.position(pos.x, pos.y))
+                    stroke = null
+                    contour(it.first)
+                    if (keyboard.pressedKeys.contains("left-shift")) {
                         stroke = ColorRGBa.BLACK.opacify(0.4)
                         lineSegments(it.second)
                     }
