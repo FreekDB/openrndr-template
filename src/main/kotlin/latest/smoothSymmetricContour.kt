70d772f9fa61fcc39d92038b4b5836e9a79f6903
Abe Pazos <abe@hamoid.com>
2021-02-22 18:38:57 +0100

    Use methods from libs

--- src/main/kotlin/latest/smoothSymmetricContour.kt
+++ src/main/kotlin/latest/smoothSymmetricContour.kt
@@ -1,14 +1,19 @@
 package latest
 
 import aBeLibs.geometry.randomPoint
+import aBeLibs.geometry.symmetrize
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.color.rgb
 import org.openrndr.draw.isolated
 import org.openrndr.extensions.Screenshots
 import org.openrndr.extra.noise.Random
 import org.openrndr.math.IntVector2
 import org.openrndr.math.Vector2
-import org.openrndr.shape.*
+import org.openrndr.shape.LineSegment
+import org.openrndr.shape.Rectangle
+import org.openrndr.shape.ShapeContour
+import org.openrndr.shape.bounds
 import java.lang.Double.min
 
 /**
@@ -21,47 +26,20 @@ import java.lang.Double.min
  */
 
 fun main() = application {
+    val side = 5
     configure {
         width = 1000
         height = 1000
     }
     program {
-        fun ShapeContour.symmetrize(roundness: (Int) -> Pair<Double, Double>):
-                Pair<ShapeContour, List<LineSegment>> {
-            val visibleTangents = mutableListOf<LineSegment>()
-            val tangents = segments.mapIndexed { i, curr ->
-                val next = segments[(i - 1 + segments.size) % segments.size]
-                (curr.direction()).mix(next.direction(), 0.5).normalized
-            }
-            val newSegments = segments.mapIndexed { i, currSegment ->
-                val sz = segments.size
-                val len = currSegment.length
-                val iNext = (i + 1 + sz) % sz
-                val c0 = currSegment.start +
-                        tangents[i] * len * roundness(i).first
-                val c1 = currSegment.end -
-                        tangents[iNext] * len * roundness((i + 1) % sz).second
-                visibleTangents.add(LineSegment(currSegment.start, c0))
-                visibleTangents.add(LineSegment(c1, currSegment.end))
-                visibleTangents.add(
-                    LineSegment(
-                        currSegment.start,
-                        currSegment.end
-                    )
-                )
-                Segment(currSegment.start, c0, c1, currSegment.end)
-            }
-            return Pair(ShapeContour(newSegments, closed), visibleTangents)
-        }
-
-        fun softies(): List<Pair<ShapeContour, List<LineSegment>>> {
+        fun createSofties(): List<Pair<ShapeContour, List<LineSegment>>> {
             do {
                 val numPoints = Random.int(6, 8)
                 val points = List(numPoints) {
                     Rectangle.fromCenter(Vector2.ZERO, 300.0).randomPoint()
                 }
                 val center = points.bounds.center
-                val maxSize = 120.0
+                val maxSize = 480.0 / side
                 val scale = min(
                     maxSize / points.bounds.width,
                     maxSize / points.bounds.height
@@ -70,38 +48,55 @@ fun main() = application {
                 val c = ShapeContour.fromPoints(pointsCentered, true)
                 // NOTE: .intersections() can return 0 intersections even if
                 // there is one: https://github.com/lacuna/artifex/issues/3
+                // NOTE2: it's checking if the source shape has intersections
+                // but not if the symmetrized versions do.
                 if (c.intersections(c).isEmpty()) {
-                    return List(16) {
-                        c.symmetrize { i ->
-                            val n =
-                                Random.simplex(it * 0.3, i * 0.3) * 0.4 + 0.5
+                    val result =
+                        mutableListOf<Pair<ShapeContour, List<LineSegment>>>()
+                    var tries = 0
+                    while (result.size < side * side) {
+                        val cSym = c.symmetrize { i ->
+                            val n = Random.simplex(
+                                result.size * 0.3,
+                                i * 0.3 + tries * 7.17
+                            ) * 0.4 + 0.5
                             Pair(n, n)
                         }
+                        if (cSym.first.intersections(cSym.first).isEmpty() ||
+                            tries++ > 5
+                        ) {
+                            result.add(cSym)
+                        }
                     }
+                    return result
                 }
             } while (true)
         }
 
-        var softies = softies()
+        var softies = createSofties()
         extend(Screenshots())
         extend {
-            drawer.clear(ColorRGBa.WHITE)
-            drawer.fill = ColorRGBa.PINK
+            drawer.clear(rgb("322E2A"))
+            drawer.fill = rgb("DCA050")
             softies.forEachIndexed { i, it ->
                 drawer.isolated {
-                    val pos = (IntVector2(i % 4, i / 4).vector2 + 1.0) / 5.0
+                    val pos = (IntVector2(
+                        i % side,
+                        i / side
+                    ).vector2 + 1.0) / (side + 1.0)
                     translate(bounds.position(pos.x, pos.y))
+                    rotate(180 * Random.simplex(pos * 0.2))
                     stroke = null
                     contour(it.first)
                     if (keyboard.pressedKeys.contains("left-shift")) {
-                        stroke = ColorRGBa.BLACK.opacify(0.4)
+                        stroke = ColorRGBa.WHITE.opacify(0.4)
                         lineSegments(it.second)
                     }
                 }
             }
         }
         mouse.buttonDown.listen {
-            softies = softies()
+            softies = createSofties()
         }
     }
 }
