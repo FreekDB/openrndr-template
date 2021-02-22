b4f86be609c9e12242c59beb004d5084d4c51c7f
Abe Pazos <abe@hamoid.com>
2021-02-22 18:29:11 +0100

    Clockwise sorted random points for cmr

--- src/main/kotlin/apps2/Catmullrom.kt
+++ src/main/kotlin/apps2/Catmullrom.kt
@@ -1,5 +1,6 @@
 package apps2
 
+import aBeLibs.geometry.randomPoint
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.extensions.Screenshots
@@ -8,17 +9,21 @@ import org.openrndr.math.Polar
 import org.openrndr.math.map
 import org.openrndr.shape.Segment
 import org.openrndr.shape.ShapeContour
+import org.openrndr.shape.toContour
 
 fun main() = application {
     program {
-        //val points = List(9) { drawer.bounds.randomPoint() }
-        val points = List(6) { Polar(it * 69.0, 200.0).cartesian + drawer.bounds.center }
-        val cmr = CatmullRomChain2(points, 0.5, loop = true)
-        val contour = ShapeContour.fromPoints(cmr.positions(200), true)
-
+        // clockwise sorting
+        val points = List(9) { drawer.bounds.randomPoint() }.sortedBy {
+            Polar.fromVector(it - drawer.bounds.center).theta
+        }
 
         extend(Screenshots())
         extend {
+            // alpha has no effect when points is evenly distributed in a circle
+            val alpha = mouse.position.x / width.toDouble()
+            val cmr = CatmullRomChain2(points, alpha, loop = true)
+            val contour = cmr.toContour()
             drawer.run {
                 clear(ColorRGBa.WHITE)
                 fill = null
@@ -26,25 +31,9 @@ fun main() = application {
                 contour(contour)
                 circles(points, 5.0)
 
-                stroke = ColorRGBa.RED
-                val a = mouse.position.x.map(0.0, width * 1.0, 100.0, 3000.0)
-                contour(cmr.toContour(a))
                 fill = ColorRGBa.BLACK
-                text(a.toString(), 50.0, 50.0)
+                text(alpha.toString(), 50.0, 50.0)
             }
         }
     }
-}
-
-fun CatmullRomChain2.toContour(v: Double): ShapeContour {
-    return ShapeContour(segments.map {
-        it.run {
-            val d = p1.distanceTo(p2)
-            val a = v / d
-            Segment(
-                p1, p1 + (p2 - p0) / a,
-                p2 - (p3 - p1) / a, p2
-            )
-        }
-    }, loop)
 }
